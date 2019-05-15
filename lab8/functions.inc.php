b34ee8191c07f7b7df94fd92e99db8214b965800
jinaxc <18302010001@fudan.edu.cn>
2019-05-15 23:36:25 +0800

    add point

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -1,12 +1,12 @@
 <?php
 
     function outputOrderRow($file, $title, $quantity, $price) {
-        $total = $price * $quantity;
+        $total = number_format($price * $quantity,2,".","");
         echo "<tr>";
         echo "<td><img src='images/books/tinysquare/".$file."'></td>";
         echo "<td class='mdl-data-table__cell--non-numeric'>".$title."</td>";
         echo "<td>".$quantity."</td>";
-        echo "<td>".$price."</td>";
+        echo "<td>".number_format($price,2,".","")."</td>";
         echo "<td>".$total."</td>";
         echo "</tr>";
     }
