9ee68b3b468249bb1340087cb381371c6e169c7d
jinaxc <18302010001@fudan.edu.cn>
2019-06-05 19:59:09 +0800

    lab11

--- lab11/Lab11.php
+++ lab11/Lab11.php
@@ -1,10 +1,65 @@
 <?php
 //Fill this place
+  $db = new Mysqli("localhost","root","ijyt0809","travel");
+  if($db->errno){
+    die("connection failed");
+  }
+  // connect mysql
+  if($_GET["continent"] !== "0" && $_GET["country"] !== "0"){
+    $continentCode = $_GET["continent"];
+    $countryISO = $_GET["country"];
+    $cities = filterByContinentAndCountry($db,$continentCode,$countryISO);
+  }
+  else if($_GET["country"] !== "0"){
+    $countryISO = $_GET["country"];
+    $cities = filterByCountry($db,$countryISO);
+  }
+  else if($_GET["continent"] !== "0"){
+    $continentCode = $_GET["continent"];
+    $cities = filterByContinent($db,$continentCode);
+  }
+  else{
+    $cities = getAllCities($db);
+  }
+  $continents = getData($db,"continents");
+  $countries = getData($db,"countries");
 
+  function getData($db,$tableName){
+    $sql = "SELECT * FROM $tableName";
+    $result = $db->query($sql);
+    return $result;
+  }
+  function getAllCities($db){
+    $sql = "SELECT * FROM ImageDetails";
+    $result = $db->query($sql);
+    return $result;
+  }
+  function filterByContinent($db,$continentCode){
+    $sql = "SELECT * FROM ImageDetails WHERE ContinentCode = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("s",$continentCode);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result;
+  }
+  function filterByCountry($db,$countryISO){
+    $sql = "SELECT * FROM ImageDetails WHERE CountryCodeISO = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("s",$countryISO);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result;
+  }
+  function filterByContinentAndCountry($db,$continentCode,$countryISO){
+    $sql = "SELECT * FROM ImageDetails WHERE ContinentCode = ? AND CountryCodeISO = ?";
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("ss",$continentCode,$countryISO);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    return $result;
+  }
 //****** Hint ******
 //connect database and fetch data here
-
-
 ?>
 
 <!DOCTYPE html>
@@ -43,14 +98,13 @@
                 <option value="0">Select Continent</option>
                 <?php
                 //Fill this place
-
+                
                 //****** Hint ******
                 //display the list of continents
 
-                while($row = $result->fetch_assoc()) {
+                while($row = $continents->fetch_assoc()) {
                   echo '<option value=' . $row['ContinentCode'] . '>' . $row['ContinentName'] . '</option>';
                 }
-
                 ?>
               </select>     
               
@@ -61,6 +115,9 @@
 
                 //****** Hint ******
                 /* display list of countries */ 
+                while($row = $countries->fetch_assoc()) {
+                  echo '<option value=' . $row['ISO'] . '>' . $row['CountryName'] . '</option>';
+                }
                 ?>
               </select>    
               <input type="text"  placeholder="Search title" class="form-control" name=title>
@@ -74,6 +131,19 @@
 
 		<ul class="caption-style-2">
             <?php 
+            while($row = $cities->fetch_assoc()){
+              echo "<li>";
+              echo "<a href='detail.php?id=".$row["ImageID"]."'class=img-responsive>";
+              echo "<img src='images/square-medium/".$row["Path"]."'alt=????>";
+              echo "<div class=caption>";
+              echo "<div class=blur></div>";
+              echo "<div class=caption-text>";
+              echo "<p>".$row["Title"]."</p>";
+              echo "</div>";
+              echo "</div>";
+              echo "</a>";
+              echo "</li>";
+            }
             //Fill this place
 
             //****** Hint ******
